4d92cbc93916aa48d11a3aafaadb10847c375fad
bagonataniel <bagonataniel@gmail.com>
2025-12-15 10:31:23 +0100

    All services updated

--- Frontend/HandyGo/src/app/services/booking.service.ts
+++ Frontend/HandyGo/src/app/services/booking.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,25 @@ import { Injectable } from '@angular/core';
 })
 export class BookingService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  public createBooking(id:any){
+    return this._http.post('http://localhost:3000/booking', id, { headers: { 'x-auth-token': localStorage.getItem('token') || '' } });
+  }
+  
+  public getBookingDetails(id:any){
+    return this._http.get(`http://localhost:3000/booking/${id}`, { headers: { 'x-auth-token': localStorage.getItem('token') || '' } });
+  }
+
+  public getClientsBookings(){
+    return this._http.get('http://localhost:3000/booking/bookings/client', { headers: { 'x-auth-token': localStorage.getItem('token') || '' } });
+  }
+
+  public getWorkerBookings(){
+    return this._http.get('http://localhost:3000/booking/bookings/worker', { headers: { 'x-auth-token': localStorage.getItem('token') || '' } });
+  }
+
+  public updateBookingStatus(id:any, status:any){
+    return this._http.put(`http://localhost:3000/booking/${id}/status`, status, { headers: { 'x-auth-token': localStorage.getItem('token') || '' } });
+  }
 }
