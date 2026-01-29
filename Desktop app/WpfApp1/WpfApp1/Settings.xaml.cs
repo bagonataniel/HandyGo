32c122286c60e915c2c58da1e10b1cfdfe828067
bagonataniel <bagonataniel@gmail.com>
2026-01-29 13:55:03 +0100

    Add Settings user control and integrate into main layout
    Introduced a new Settings.xaml and Settings.xaml.cs user control for admin management. Updated MainLayout.xaml.cs to load the Settings control when the settings button is clicked. Adjusted project and document layout files to include the new Settings control.

---
+++ Desktop app/WpfApp1/WpfApp1/Settings.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : UserControl
+    {
+        private string token;
+        HttpClient client = new HttpClient();
+        public Settings(string TOKEN)
+        {
+            InitializeComponent();
+            DataContext = this;
+            token = TOKEN;
+            client.DefaultRequestHeaders.Add("x-admin-auth-token", token);
+            //_ = apiCall();
+        }
+    }
+}
