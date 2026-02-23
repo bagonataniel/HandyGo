746d7857623f29f6908dd86124ef476cdfa92f8e
bagonataniel <bagonataniel@gmail.com>
2026-02-23 10:29:23 +0100

    Add Folder publish profile and update VS metadata
    Add a FolderProfile.pubxml to configure Release publish settings (net8.0-windows, win-x64, self-contained). Update the .csproj.user to reference the new publish profile and bump AssemblyInformationalVersion (and its cache). Remove the HandyGoAdminSetup.exe binary and apply minor Visual Studio layout (.suo/DocumentLayout) adjustments.

--- Desktop app/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ Desktop app/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b672da1158198925e462ce6ffe3debcc92ebd634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eccb94cc83fcdcd2fa271fd7099e1aa009317f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
