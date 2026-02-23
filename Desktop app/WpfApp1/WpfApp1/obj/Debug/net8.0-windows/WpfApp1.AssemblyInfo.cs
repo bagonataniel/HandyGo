9eccb94cc83fcdcd2fa271fd7099e1aa009317f6
bagonataniel <bagonataniel@gmail.com>
2026-02-23 09:53:03 +0100

    Add WPF app icon; move frontend logo
    Add Resources/logo.ico and set the MainWindow Icon attribute; update WpfApp1.csproj to include the icon as a Resource. Move newlogo.png from Frontend/HandyGo/src/assets to Frontend/HandyGo/public and update index.html accordingly. Several IDE/.vs and build artefacts (obj/bin/generated .cs/.dll/.pdb and DocumentLayout metadata) were updated as a result of the change and rebuild.

--- Desktop app/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ Desktop app/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd6ff71a9b1519fc1a689c0bd98510a4593837e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b672da1158198925e462ce6ffe3debcc92ebd634")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
