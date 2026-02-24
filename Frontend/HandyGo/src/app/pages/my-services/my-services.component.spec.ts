d560cca24a4646ac011923f4030d7ae02b0e8ed1
Gungl Benjamin <gungl.benjamin@gmail.com>
2026-02-24 10:26:14 +0100

    fixed some tests

--- Frontend/HandyGo/src/app/pages/my-services/my-services.component.spec.ts
+++ Frontend/HandyGo/src/app/pages/my-services/my-services.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MyServicesComponent } from './my-services.component';
 
@@ -8,7 +9,8 @@ describe('MyServicesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MyServicesComponent]
+      declarations: [MyServicesComponent],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
 
